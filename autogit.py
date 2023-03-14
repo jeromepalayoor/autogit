55cd594f2d85a0ce10f0d07d4b950158e03cc7a4
Jerome Palayoor <63996033+jeromepalayoor@users.noreply.github.com>
2023-03-14 12:42:14 +0800

    Update autogit.py

--- autogit.py
+++ autogit.py
@@ -1,4 +1,8 @@
 import subprocess
+import sys
 subprocess.call(["git", "add", "."])
-subprocess.call(["git", "commit", "-m", "'updated stuff'"])
+if len(sys.argv) > 1:
+    subprocess.call(["git", "commit", "-m", f"{sys.argv[1]}"])
+else:
+    subprocess.call(["git", "commit", "-m", "'updated stuff'"])
 subprocess.call(["git", "push", "origin", "main"])
